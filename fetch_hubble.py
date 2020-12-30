7bb7ee3b1406b575ce5025e73e718458c9d38bd0
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2020-12-31 00:28:45 +0300

    refactor
    * delete unnecessary code
    * rename variable

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -3,15 +3,6 @@ import os
 from utils import load_img_with_pillow
 
 
-def get_hubble_image_ids():
-    url = "http://hubblesite.org/api/v3/images/all"
-    response = requests.get(url=url)
-    response.raise_for_status()
-    links = response.json()
-    image_ids = [link['id'] for link in links]
-    return image_ids
-
-
 def get_hubble_image_by_id(img_id: int, path: str):
     url = f"http://hubblesite.org/api/v3/image/{img_id}"
     name = f'hubble_image_{img_id}'
@@ -27,7 +18,7 @@ def get_hubble_images_from_collection(collection_name, path):
     url = f"http://hubblesite.org/api/v3/images/{collection_name}"
     response = requests.get(url=url)
     response.raise_for_status()
-    images_dict = response.json()
-    image_ids = [image_json["id"] for image_json in images_dict]
+    images = response.json()
+    image_ids = [image_json["id"] for image_json in images]
     for image_id in image_ids:
         get_hubble_image_by_id(img_id=image_id, path=path)
